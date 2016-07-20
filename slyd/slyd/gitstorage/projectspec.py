543533b0a4452f039f107c250c540697dfaccec5
Ruairi Fahy <ruairifahy91@gmail.com>
2016-07-20 12:28:30 +0100

    Schedule a spider to run from portia.
    Add button to schedule crawl from the UI
    Add schedule and _schedule_data methods to projectspec for scrapyd scheduler
    Add suport for getting objects from mysql backend with partial ids
    Add support for downloading individual branch or individual commit of project

--- slyd/slyd/gitstorage/projectspec.py
+++ slyd/slyd/gitstorage/projectspec.py
@@ -3,4 +3,19 @@ from slyd.gitstorage.projects import GitProjectMixin
 
 
 class GitProjectSpec(GitProjectMixin, ProjectSpec):
-    pass
+    def _schedule_data(self, spider, args):
+        branch = args.pop('branch', [None])[0]
+        commit = args.pop('commit_id', [None])[0]
+        project = self.project_name
+        arg = {}
+        if commit:
+            arg['commit'] = commit
+        elif branch:
+            arg['branch'] = branch
+        self._checkout_commit_or_head(project, **arg)
+        commit_id = self.storage._commit.id
+        return {
+            'project': self.project_name,
+            'version': commit_id,
+            'spider': spider
+        }
